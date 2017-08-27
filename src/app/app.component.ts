13a885eb48851efd1689b84c29e6d8c353c6cd04
Steve Witman <stevewitman@gmail.com>
2017-08-26 22:32:22 -0600

    End of example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/auth.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  user = null;
+  links: FirebaseListObservable<any[]>;
+  constructor(
+              private auth: AuthService,
+              public db: AngularFireDatabase) { }
+
+  ngOnInit() {
+    this.auth.getAuthState().subscribe(
+      (user) => this.user = user);
+    this.links = this.db.list('/links')
+  }
+
+  loginWithGoogle() {
+    this.auth.loginWithGoogle();
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
+import { AuthService } from './shared/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
